77f3fbe463489d05bdd457bf0bb4dfe0b544786a
FilipPerkovic <fliper0411@gmail.com>
2026-06-21 19:02:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UpravljanjeZgradama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Aplikacija za upravljanje zgradama");
+        }
+    }
+}
